3b4f59a16afebfeaab395fb952f956512fd82d85
Lee Geunjeong <kjlee@nexell.co.kr>
2019-11-27 11:06:54 +0900

    nx-gl-tools: update gl library ver 1.2.1
    This patch supports 64bit gl library.
    
    Signed-off-by: Lee Geunjeong <kjlee@nexell.co.kr>
    Change-Id: I96e065505af44292182a6d2bed2f4384f694ecf2
    Reviewed-on: https://review.gerrithub.io/c/NexellCorp/linux_library_nx-gl-tools/+/475939

--- autogen.sh
+++ autogen.sh
@@ -10,5 +10,5 @@ autoreconf --force --verbose --install || exit 1
 cd "$ORIGDIR" || exit $?
 
 if test -z "$NOCONFIGURE"; then
-    "$srcdir"/configure --host=arm AR_FLAGS="crU""$@"
+    "$srcdir"/configure AR_FLAGS="crU""$@"
 fi
